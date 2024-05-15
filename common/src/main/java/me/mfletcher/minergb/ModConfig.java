176026e50d797dda5abd885c7072bbd18dbdc306
mfletcher2 <92177225+mfletcher2@users.noreply.github.com>
2024-05-15 15:58:23 -0700

    Add some cloth config settings

--- common/src/main/java/me/mfletcher/minergb/ModConfig.java
+++ common/src/main/java/me/mfletcher/minergb/ModConfig.java
@@ -1,9 +1,18 @@
-
 package me.mfletcher.minergb;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Config(name = MineRGB.MOD_ID)
 public class ModConfig implements ConfigData {
+    @ConfigEntry.Gui.Tooltip
+    List<String> disabledDevices = new ArrayList<>();
+
+    boolean damageFlash = true;
+
+    boolean xpFlash = true;
 }
