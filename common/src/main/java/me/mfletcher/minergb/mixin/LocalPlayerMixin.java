4e12a05cd4965d9ca9380c056a58479f1480b408
mfletcher2 <92177225+mfletcher2@users.noreply.github.com>
2024-05-16 14:54:19 -0700

    Change Player mixin to LocalPlayer

--- common/src/main/java/me/mfletcher/minergb/mixin/LocalPlayerMixin.java
+++ common/src/main/java/me/mfletcher/minergb/mixin/LocalPlayerMixin.java
@@ -1,6 +1,10 @@
 package me.mfletcher.minergb.mixin;
 
+import com.mojang.authlib.GameProfile;
 import me.mfletcher.minergb.MineRGBClientController;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -12,27 +16,27 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Player.class)
-public abstract class PlayerMixin extends LivingEntity {
-    protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
-        super(entityType, level);
+@Mixin(LocalPlayer.class)
+public abstract class LocalPlayerMixin extends AbstractClientPlayer {
+
+    public LocalPlayerMixin(ClientLevel clientLevel, GameProfile gameProfile) {
+        super(clientLevel, gameProfile);
     }
 
     @Inject(method = "hurt", at = @At("TAIL"))
     public void onHurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-//        new Thread(MineRGBClientController::flashHurt).start();
         MineRGBClientController.setHurt();
-//        MineRGBClientController.setHealth(getHealth(), getMaxHealth());
     }
 
     @Override
     public void heal(float healAmount) {
         super.heal(healAmount);
-//        MineRGBClientController.setHealth(getHealth(), getMaxHealth());
     }
 
-    @Inject(method = "giveExperiencePoints", at = @At("TAIL"))
-    public void onGiveExperiencePoints(int xpPoints, CallbackInfo ci) {
+
+    @Override
+    public void giveExperiencePoints(int xpPoints) {
+        super.giveExperiencePoints(xpPoints);
         MineRGBClientController.setXp();
     }
 }
