c314c2797937a0e49deec0122ba40375be0222ce
mfletcher2 <92177225+mfletcher2@users.noreply.github.com>
2024-05-16 16:55:56 -0700

    Use SetExperienceValues for when xp changes

--- common/src/main/java/me/mfletcher/minergb/mixin/LocalPlayerMixin.java
+++ common/src/main/java/me/mfletcher/minergb/mixin/LocalPlayerMixin.java
@@ -6,10 +6,6 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -33,10 +29,8 @@ public abstract class LocalPlayerMixin extends AbstractClientPlayer {
         super.heal(healAmount);
     }
 
-
-    @Override
-    public void giveExperiencePoints(int xpPoints) {
-        super.giveExperiencePoints(xpPoints);
+    @Inject(method = "setExperienceValues", at = @At("TAIL"))
+    public void onSetExperienceValues(float currentXP, int maxXP, int level, CallbackInfo ci) {
         MineRGBClientController.setXp();
     }
 }
